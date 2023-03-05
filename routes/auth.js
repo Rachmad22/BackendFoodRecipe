b4e69efdd4842e1f4123003fc828bc30f0f73325
Rachmad NH <116181515+Rachmad22@users.noreply.github.com>
2023-03-05 17:29:47 +0700

    [ADD] endpoint signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
-const { validateLogin } = require('../middlewares/validation')
+const { validateLogin, validateCreateUser } = require('../middlewares/validation')
 const authController = require('../controllers/login')
+const userController = require('../controllers/users')
 
+router.post('/signup', validateCreateUser, userController.createUser)
 
 router.post('/login', validateLogin, authController.login)
 
